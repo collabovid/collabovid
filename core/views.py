70f8a339ad84ab9cf6dcbbf666a8442665542cee
Michael Perk <y0079915@tu-braunschweig.de>
2020-03-28 02:07:24 +0100

    Moved Description to about, some small design changes

--- core/views.py
+++ core/views.py
@@ -3,7 +3,15 @@ from django.templatetags.static import static
 
 
 def home(request):
-    return render(request, "core/home.html")
+    card = dict(
+        title='Tracing DAY-ZERO and Forecasting the Fade out of the COVID-19 Outbreak in Lombardy, Italy: A Compartmental Modelling and Numerical Optimization Approach.',
+        date='March 26, 2020',
+        description='Objectives: The rapidly evolving coronavirus disease 2019 (COVID-19), was declared a pandemic by the World Health Organization on March 11, 2020. It was first detected in the city of Wuhan in China and has spread globally resulting in substantial health and economic crisis in many countries.',
+        pdf='#',
+        ext='#',
+        img_src=str(static('core/img/paper-sample.png'))
+    )
+    return render(request, "core/home.html", {'card': card})
 
 
 def card_demo(request):
