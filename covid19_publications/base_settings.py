004d9ffef3dba6a7df2f73693aeb1fae8ce30bc0
Jonas Dippel <jonas.dippel@gmx.de>
2020-03-29 23:18:47 +0200

    updated settings and removed lda

--- covid19_publications/base_settings.py
+++ covid19_publications/base_settings.py
@@ -63,16 +63,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'covid19_publications.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
--- covid19_publications/prod_settings.py
+++ covid19_publications/prod_settings.py
@@ -2,3 +2,14 @@ from .base_settings import *
 
 ALLOWED_HOSTS = ['django-env.eba-3yrfmfbq.us-east-1.elasticbeanstalk.com']
 SECRET_KEY = os.environ('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
+    }
+}
