d32c1bf07a0084a6e30bb26566c790ef89bf5ee8
Yannic Lieder <y.lieder@gmx.de>
2020-03-29 17:38:07 +0200

    Add update buttons for papers and PDF thumbnails to admin console

--- scrape/admin.py
+++ scrape/admin.py
@@ -23,3 +23,25 @@ class AuthorAdmin(admin.ModelAdmin):
         citation_refresher.refresh_citations()
         self.message_user(request, "All citations inserted")
         return HttpResponseRedirect("../")
+
+@admin.register(Paper)
+class PaperAdmin(admin.ModelAdmin):
+    change_list_template = "scrape/paper_changelist.html"
+
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path('update_papers/', self.refresh_papers),
+            path('update_images/', self.refresh_images),
+        ]
+        return my_urls + urls
+
+    def refresh_papers(self, request):
+        # todo
+        self.message_user(request, "Papers updated")
+        return HttpResponseRedirect("../")
+
+    def refresh_images(self, request):
+        # todo
+        self.message_user(request, "PDF thumbnails updated")
+        return HttpResponseRedirect("../")
