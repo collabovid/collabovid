7024d4baa88d389968cd7d50e7b074d30a472fe3
Moritz Pfister <MoritzPfister@gmx.de>
2020-03-29 18:10:29 +0200

    Modified setup script for initial image downloads.

--- setup/image_downloader.py
+++ setup/image_downloader.py
@@ -1,12 +1,13 @@
+# Note: Run this script from the top level directory (covid19-publications)
+
 import os
 import requests
 import shutil
 
-from django.conf import settings
 from zipfile import ZipFile
 
 
-class ImageDownloader:
+if __name__ == "__main__":
     DOWNLOAD_URL = 'https://dl.dropboxusercontent.com/s/ypzbzlb60b8acmc/pdf_images.zip'
 
     IMAGES_FOLDER = 'pdf_images'
@@ -17,26 +18,25 @@ class ImageDownloader:
     FILE_PATH = os.path.join(TMP_DIR, TMP_FILENAME)
     EXTRACTED_FOLDER_PATH = os.path.join(TMP_DIR, IMAGES_FOLDER)
 
-    def download_images(self):
-        if os.path.isdir(settings.PDF_IMAGE_FOLDER):
-            print(f'Removing current image folder {settings.PDF_IMAGE_FOLDER}')
-            shutil.rmtree(settings.PDF_IMAGE_FOLDER)
+    if os.path.isdir(IMAGES_FOLDER):
+        print(f'Removing current image folder {IMAGES_FOLDER}')
+        shutil.rmtree(IMAGES_FOLDER)
 
-        if not os.path.isdir("tmp"):
-            print('Creating tmp directory')
-            os.mkdir('tmp')
+    if not os.path.isdir(TMP_DIR):
+        print('Creating tmp directory')
+        os.mkdir(TMP_DIR)
 
-        print('Downloading zip file.')
-        r = requests.get(self.DOWNLOAD_URL)
+    print('Downloading zip file.')
+    r = requests.get(DOWNLOAD_URL)
 
-        with open(self.FILE_PATH, 'wb') as f:
-            f.write(r.content)
-        print('Download complete. Extracting..')
+    with open(FILE_PATH, 'wb') as f:
+        f.write(r.content)
+    print('Download complete. Extracting..')
 
-        with ZipFile(self.FILE_PATH, 'r') as z:
-            z.extractall(path=self.TMP_DIR)
+    with ZipFile(FILE_PATH, 'r') as z:
+        z.extractall(path=TMP_DIR)
 
-        print(f'Moving {self.EXTRACTED_FOLDER_PATH} to {settings.BASE_DIR}')
-        shutil.move(self.EXTRACTED_FOLDER_PATH, settings.BASE_DIR)
-        print(f'Cleaning up: Removing temp directory {self.TMP_DIR}')
-        shutil.rmtree(self.TMP_DIR)
+    print(f'Moving {EXTRACTED_FOLDER_PATH} to {IMAGES_FOLDER}')
+    shutil.move(EXTRACTED_FOLDER_PATH, IMAGES_FOLDER)
+    print(f'Cleaning up: Removing temp directory {TMP_DIR}')
+    shutil.rmtree(TMP_DIR)
