4158e67f5de4e0f82bafeea0df08e2b4130a7e9f
Jonas Dippel <jonas.dippel@gmx.de>
2020-03-30 16:35:26 +0200

    added debug to prod settings for testing

--- covid19_publications/prod_settings.py
+++ covid19_publications/prod_settings.py
@@ -5,6 +5,8 @@ INSTALLED_APPS.append('storages')
 ALLOWED_HOSTS = ['django-env.eba-3yrfmfbq.us-east-1.elasticbeanstalk.com', 'collabovid.org']
 SECRET_KEY = os.environ['SECRET_KEY']
 
+DEBUG=True
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
