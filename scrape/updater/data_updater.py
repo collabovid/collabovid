7764021ef529ef656a873bea0776cb77a32db6c2
Yannic Lieder <y.lieder@gmx.de>
2020-05-02 18:53:15 +0200

    Fix bug in covid-19 related check

--- scrape/updater/data_updater.py
+++ scrape/updater/data_updater.py
@@ -93,7 +93,7 @@ class ArticleDataPoint(object):
 
     @staticmethod
     def _covid_related(db_article):
-        if db_article.published_at < date(year=2019, month=12, day=1):
+        if db_article.published_at and db_article.published_at < date(year=2019, month=12, day=1):
             return False
 
         _COVID19_KEYWORDS = r'(corona.?virus|(^|\s)corona(\s|$)|covid.?19|(^|\s)covid(\s|$)|sars.?cov.?2|2019.?ncov)'
