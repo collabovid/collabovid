95857458df97ce77a2dfbbbc88cf0ebfe55126bc
Yannic Lieder <y.lieder@gmx.de>
2020-05-11 14:37:15 +0200

    Create script to set all null values in paper.data_source to medRxiv datasource

---
+++ setup/update_datasource_null_values.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    import django
+    django.setup()
+
+    from data.models import Paper, DataSource
+
+    print("Set all null values in datasource field to medRxiv datasource")
+    papers = Paper.objects.filter(data_source=None)
+    datasource, _ = DataSource.objects.get_or_create(DataSource.MEDBIORXIV_DATASOURCE_NAME)
+    for paper in papers:
+        paper.data_source = datasource
+        paper.save()
