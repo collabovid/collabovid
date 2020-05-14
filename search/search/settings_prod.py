d60d2d71f7863f485fd213418629182acd823bcb
Jonas Dippel <jonas.dippel@gmx.de>
2020-05-14 20:51:47 +0200

    added pod ip to allowed hosts in search

--- search/search/settings_prod.py
+++ search/search/settings_prod.py
@@ -4,4 +4,10 @@ from collabovid_settings.postgres_settings import *
 from collabovid_settings.tasks_settings import *
 
 ALLOWED_HOSTS += [SEARCH_SERVICE_HOST]
+
+POD_IP = os.getenv('POD_IP', None)
+if POD_IP:
+    ALLOWED_HOSTS += [POD_IP]
+
+
 SECRET_KEY = os.getenv('SECRET_KEY', '7_%)-$#43s2hk6e6)ip)4+*3d6vz&73%zqos7un9qkz$2pt@h*')
