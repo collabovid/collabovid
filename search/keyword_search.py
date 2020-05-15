e82b3308e261225a9983d323f1c93ae898310336
Moritz Pfister <m.pfister@tu-bs.de>
2020-05-15 19:29:26 +0200

    Added ability to search for author's lastnames in a search query.

--- search/keyword_search.py
+++ search/keyword_search.py
@@ -1,4 +1,6 @@
 import os
+import re
+from collections import defaultdict
 
 import nltk
 from django.db.models import Q
@@ -7,9 +9,9 @@ from search.search import Search, PaperResult
 from typing import List
 from data.models import Paper
 
-
-KEYWORD_BASE_SCORE = 0.8
-
+AUTHOR_ADDITIVE_SCORE = 0.5  # Score per matching author
+TITLE_ADDITIVE_SCORE = 0.2  # Score per matching keyword in title
+ABSTRACT_ADDITIVE_SCORE = 0.1  # Score per matching keyword in abstract
 
 # Download nltk stopwords, if not exist
 try:
@@ -24,9 +26,13 @@ stopwords = set(nltk.corpus.stopwords.words('english'))
 
 
 class KeywordSearch(Search):
+    @staticmethod
+    def _get_score(summed_score, word_count):
+        return min(1, summed_score / (word_count * TITLE_ADDITIVE_SCORE))
+
     def find(self, query: str) -> List[PaperResult]:
         """
-        Search for records, containing all keywords of query. Stopwords are excluded before. If all words in query are
+        Search for records, containing all keywords of query, either in the title, abstract, or an authors lastname. Stopwords are excluded before. If all words in query are
         stopwords, use stopwords instead.
 
         :param query: Search query.
@@ -37,10 +43,27 @@ class KeywordSearch(Search):
             # In case that query consists of stopwords only, use stopwords for search
             keywords = query.split()
 
-        sql_query = Q()
+        filtered_papers = Paper.objects.all()
         for word in keywords:
-            # Each keyword must be contained in title OR in abstract
-            sql_query &= (Q(title__icontains=word))
+            filtered_papers = filtered_papers.filter((Q(title__icontains=word) | Q(abstract__icontains=word) | Q(authors__last_name__iexact=word)))
 
-        paper_dois = Paper.objects.filter(sql_query).values_list('doi', flat=True)
-        return [PaperResult(paper_doi=doi, score=KEYWORD_BASE_SCORE) for doi in paper_dois]
+        filtered_papers = filtered_papers.distinct()
+
+        # Evaluate the individual paper scores based on the matches
+        summed_scores = defaultdict(int)
+        for paper in filtered_papers:
+              for word in keywords:
+                # We only count the first type of match (which gives the most points). So we do not count any word twice
+                author_matched = False
+                for author in paper.authors.all():
+                    if author.last_name.lower() == word.lower():
+                        summed_scores[paper.doi] += AUTHOR_ADDITIVE_SCORE
+                        author_matched = True
+                        break
+                if author_matched:
+                    continue
+                if re.search(word, paper.title, re.IGNORECASE):
+                    summed_scores[paper.doi] += TITLE_ADDITIVE_SCORE
+                elif re.search(word, paper.abstract, re.IGNORECASE):
+                    summed_scores[paper.doi] += ABSTRACT_ADDITIVE_SCORE
+        return [PaperResult(paper_doi=paper.doi, score=KeywordSearch._get_score(summed_scores[paper.doi], len(keywords))) for paper in filtered_papers]
