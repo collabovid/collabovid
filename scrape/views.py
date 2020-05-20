4695df744540bc5ca950642f77be508fccec08c8
Moritz Pfister <m.pfister@tu-bs.de>
2020-05-20 14:17:02 +0200

    Implement data-sanitizing via POST-requests.

--- scrape/views.py
+++ scrape/views.py
@@ -1,7 +1,6 @@
-from django.contrib.admin.views.decorators import staff_member_required
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponse
 from django.shortcuts import render
-
+from django.contrib.admin.views.decorators import staff_member_required
 from data.models import Paper, PaperState
 
 
@@ -12,16 +11,19 @@ def data_sanitizing(request):
         return render(request, "scrape/paper_table.html", {"papers": papers})
     elif request.method == "POST":
         doi = request.POST.get('doi', '')
-        new_state = request.POST.get('state', '')
+        action = request.POST.get('action', '')
 
-        if doi and new_state:
+        if doi and action:
             try:
                 paper = Paper.objects.get(doi=doi)
-                if new_state in [PaperState.UNKNOWN, PaperState.VERIFIED, PaperState.BULLSHIT]:
-                    # paper.paper_state = new_state
-                    print(f"Set state of {paper.doi} to {new_state}")
+                valid_actions = {'unknown': PaperState.UNKNOWN, 'accept': PaperState.VERIFIED,
+                                 'decline': PaperState.BULLSHIT}
+                if action in valid_actions:
+                    print(f"Set state of {paper.doi} to {action}")
+                    paper.paper_state = valid_actions[action]
                     return HttpResponse('')
                 else:
-                    return HttpResponseNotFound('Invalid state')
+                    return HttpResponseNotFound('Invalid action')
             except Paper.DoesNotExist:
                 return HttpResponseNotFound('DOI not found.')
+        return HttpResponseNotFound
