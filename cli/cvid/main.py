3a4e6afc18ef6c5e16b8037307b6b320ede65857
Michael Perk <y0079915@tu-braunschweig.de>
2020-05-20 20:59:33 +0200

    Share Config Basic functionality. No subgit included yet.

--- cli/cvid/main.py
+++ cli/cvid/main.py
@@ -13,6 +13,7 @@ from cvid.commands.release import ReleaseCommand
 from cvid.commands.version import VersionCommand
 from cvid.commands.register import RegisterCommand
 from cvid.commands.configure_k8s import ConfigureKubernetes
+from cvid.commands.share_config import ShareConfigCommand
 import json
 from os.path import join, dirname, realpath
 import os
@@ -41,7 +42,7 @@ def main():
                 ClusterCommand(**args), JobsCommand(**args), AWSRegistryLoginCommand(**args),
                 CronJobsCommand(**args), CollectTasksCommand(**args), ReleaseCommand(**args), VersionCommand(**args),
                 ConfigureKubernetes(**args), RegisterCommand(**args),
-                ModelsCommand(**args), PaperMatricesCommand(**args)]
+                ModelsCommand(**args), PaperMatricesCommand(**args), ShareConfigCommand(**args)]
 
     parser = argparse.ArgumentParser(prog='cvid')
     subparsers = parser.add_subparsers()
