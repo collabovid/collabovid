dd238794d9cf80779cbd97c3e74b2dc09c9de363
Michael Perk <y0079915@tu-braunschweig.de>
2020-05-21 14:07:25 +0200

    Added data models to the admin panel

--- web/core/admin.py
+++ web/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from data.models import Category, Paper, Topic, PaperHost, DataSource, Author
 
-# Register your models here.
+admin.site.register(Category)
+admin.site.register(Paper)
+admin.site.register(Topic)
+admin.site.register(PaperHost)
+admin.site.register(DataSource)
+admin.site.register(Author)
