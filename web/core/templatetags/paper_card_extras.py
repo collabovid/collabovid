89e723780f8727119a81558f76b282c28703c4bf
Michael Perk <y0079915@tu-braunschweig.de>
2020-05-29 17:08:08 +0200

    Paper Card larger preview on missing image. Fixed some minor style issues in search

--- web/core/templatetags/paper_card_extras.py
+++ web/core/templatetags/paper_card_extras.py
@@ -1,8 +1,19 @@
 from django import template
 register = template.Library()
 
+SLICE_DIFFERENCE = 20
+
 
 @register.filter
 def abstract_preview_size(paper):
-    print(paper.preview_image)
-    return 420 if paper.preview_image else 900
+    return 420 if paper.preview_image and len(paper.preview_image) > 0 else 900
+
+
+@register.filter
+def preview_slice(paper):
+    return paper.abstract[:abstract_preview_size(paper)-SLICE_DIFFERENCE]
+
+
+@register.filter
+def full_text_slice(paper):
+    return paper.abstract[abstract_preview_size(paper)-SLICE_DIFFERENCE:]
