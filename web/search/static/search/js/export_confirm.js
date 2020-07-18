44dae9fa15de25dfda83af946de1fbac279b7d5a
Michael Perk <y0079915@tu-braunschweig.de>
2020-07-18 17:31:18 +0200

    gloablized export function

--- web/search/static/search/js/export_confirm.js
+++ web/search/static/search/js/export_confirm.js
@@ -1,9 +1,9 @@
-function show_confirm_export_dialog() {
+window.show_confirm_export_dialog = function(){
     return confirm('Please be aware that the ordering of the authors in the exported file might be incorrect. ' +
         'We are working hard to fix this issue and will release an updated version soon.');
 
-}
+};
 
 $(document).on('click', '.export-dropdown a', function () {
-    return show_confirm_export_dialog();
+    return window.show_confirm_export_dialog();
 });
