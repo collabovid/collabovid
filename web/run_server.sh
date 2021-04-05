13900510775dc5748705e14a47717e044f399f32
Jonas Dippel <jonas.dippel@gmx.de>
2021-04-05 23:05:09 +0200

    made loading indicator white

--- web/run_server.sh
+++ web/run_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 chmod 777 -R /cache
 chown -R root:root /cache
-gunicorn ${PROJECT_NAME}.wsgi --user www-data --bind 0.0.0.0:8000 --workers 5 --timeout 500 & nginx -g "daemon off;"
+gunicorn ${PROJECT_NAME}.wsgi --user www-data --bind 0.0.0.0:8000 --workers 1 --timeout 500 & nginx -g "daemon off;"
