e949d303c35f4b02b153dabecfd8f058041343e7
Jonas Dippel <jonas.dippel@gmx.de>
2022-02-16 16:24:03 +0100

    added job to set all papers vectorized

---
+++ web/scripts/set_vectorized.py
@@ -0,0 +1,19 @@
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings_dev')
+
+import django
+
+django.setup()
+
+from data.models import Author, Paper
+
+
+def set_vectorized():
+    Paper.objects.all().update(vectorized=True)
+
+
+if __name__ == '__main__':
+    print('setting all paers to vectorized=True')
+    set_vectorized()
+    print('Done with update.')
